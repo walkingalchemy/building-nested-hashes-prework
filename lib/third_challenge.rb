25228f26077bdc1fce0abbd53edfd4dfb69857b0
James Royer <walkingalchemy@gmail.com>
2017-10-26 21:38:12 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,25 +1,37 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { 
+        name: "",
+        age: ""
         #your key/value pairs here
         },
       :matriarch => {
+        name: "",
+        age: ""
         #your key/value pairs here
         },
       :hero => {
+        name: ""
+        age: ""
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
+        name: ""
+        age: ""
         #your key/value pairs here
         },
       :matriarch => {
+        name: ""
+        age: ""
         #your key/value pairs here
         },
       :heroine => {
+        name: ""
+        age: ""
         #your key/value pairs here
         },
       :heroine_friends => []
