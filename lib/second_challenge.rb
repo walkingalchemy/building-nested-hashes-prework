6310d2cd12c04ba156c39269f3fa609bd9228124
James Royer <walkingalchemy@gmail.com>
2017-10-26 21:33:43 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,11 +3,11 @@ def second_challenge
     :montague => {
       patriarch: {}, matriarch: {}, hero:, hero_friends: {}
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: {}
       #your key/value pairs here!
       }}
 
-  
+
 end
