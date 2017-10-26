456c304d193bbc1c79a1dd31cd688756b1bf2a12
James Royer <walkingalchemy@gmail.com>
2017-10-26 21:44:57 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
           name: "Benvolio",
           age: "17",
           attitude: "worried"
-        }
+        },
         {
           name: "Mercutio",
           age: "18",
@@ -27,7 +27,7 @@ def fourth_challenge
           name: "Steven",
           age: "30",
           attitude: "confused"
-        }
+        },
         {
           name: "Nurse",
           age: "44",
